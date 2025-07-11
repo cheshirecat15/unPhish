a97c9fcbae79eb4de7537f1993786672d0c546eb
cheshirecat15 <rijuta.titir@gmail.com>
2025-07-12 00:18:48 +0530

    Made the extension stronger by adding more conditions

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 function isPhishing(url){
     const ipRegex= /^http(s)?:\/\/(\d{1,3}\.){3}\d{1,3}/;
-    const susKeywords=['login','verify','secure'];
+    const susKeywords=['login','verify','secure', 'update','password','account','bank','gift','free','claim','wallet'];
     const susTLDs=['.xyz','.tk','.top','.ru','.cn'];
 
     try{
@@ -8,27 +8,41 @@ function isPhishing(url){
 
         //check for ip address in url
         if (ipRegex.test(url)) {
-            return "URL uses an IP address";
+            return {level: "danger", message:"URL uses an IP address"};//level : "danger"
         }
+
+        //for homograph attack
+        if (parsed.hostname.includes('xn--')) {
+            return {level: "danger", message: "Potential IDN homograph attack (xn--domain)"};//level:"danger"
+            }
+
+        //check for suspicious keywords
+        for (const keyword of susKeywords){
+            const lowerPath=parsed.pathname.toLowerCase();
+            const lowerHost=parsed.hostname.toLowerCase();
+            if (lowerPath.includes(keyword)||lowerHost.includes(keyword)){
+                return {level:"danger", message:`Suspicious keyword in path: ${keyword}`};//level: "danger"
+            }
+        }
+
+        //check if not https
+        if (parsed.protocol !== 'https:'){
+            return {level: "medium", message:"Site is not using HTTPS"};//level:"medium"
+            }
+
 
         //check for suspicious TLDs
         for (const tld of susTLDs){
             if (parsed.hostname.endsWith(tld)) {
-                return `Suspicious TLD: ${tld}`;
-            }
-        }
-        //check for suspicious keywords
-        for (const keyword of susKeywords){
-            if (parsed.pathname.toLowerCase().includes(keyword)){
-                return `Suspicious keyword in path: ${keyword}`;
+                return {level:"medium", message:`Suspicious TLD: ${tld}`};//level: "medium"
             }
         }
 
         //safe url declaration
-        return null;
+        return {level: "safe", message: "No suspicious indicators detected"};//level: "safe"
     }
     catch (err){
-        return "Invalid URL";
+        return {level:"danger", message:"Invalid URL"};
     }
 }
 
@@ -39,16 +53,19 @@ chrome.tabs.query({active: true,currentWindow: true }, function(tabs){//tabs arr
     const url= tabs[0].url;//the url of the element as a string is assigned to constant variable 'url'
     document.getElementById("url").textContent =url;
     
-    const reason= isPhishing(url);//fetch the reason for safe or danger
+    const result= isPhishing(url);//fetch the result(safe, medium, danger)
     const status= document.getElementById("status");//variable status grabs <div id="status"> where we'll show the result
 
-    if(reason){
-        status.textContent= `Warning: ${reason}`;
-        status.classList.remove("safe");
-        status.classList.add("danger");
+    if(result.level=="danger"){
+        status.textContent= `Danger: ${result.message}`;
+        status.className ="status danger"
+    }
+    else if(result.level=="medium"){
+        status.textContent=`Caution: ${result.message}`;
+        status.className="status medium";
     }
     else{
         status.textContent="Safe Site";
-        status.classList.add("safe");
+        status.className="status safe";
     }
 });
